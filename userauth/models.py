342e0babdb29766f5374d3f0b90c894a62cd34f8
dabz <d.mwape@alustudent.com>
2024-06-20 01:05:31 +0300

    fixing image upload

--- userauth/models.py
+++ userauth/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField(primary_key=True, default=0)
     bio = models.TextField(blank=True, default='')
-    profileing = models.ImageField(upload_to='profile_images', default='profile-picturePlaceholder.jpg')
+    profileimg = models.ImageField(upload_to='profile_images', default='profile-picturePlaceholder.jpg')
     location = models.CharField(max_length=100, blank=True, default='')
 
     def __str__(self):
--- userauth/views.py
+++ userauth/views.py
@@ -57,10 +57,10 @@ def logoutt(request):
 
 def home(request):
     story = Story.objects.all().order_by('-created_at')
-    # profile = Profile.objects.get(user=request.user)
+    profile = Profile.objects.get(user=request.user)
     context = {
         'story': story,
-        # 'Profile': profile,
+        'Profile': profile,
     }
     return render(request, 'main.html', context)
 
@@ -116,20 +116,21 @@ def home_story(request, id):
 
 def explore(request):
     stories = Story.objects.all().order_by('-created_at')  # Corrected ordering by 'created_at'
-    # profile = Profile.objects.get(user=request.user)
+    profile = Profile.objects.get(user=request.user)
     context = {
         'stories': stories,  # Renamed 'story' to 'stories' for clarity (plural form)
-        # 'profile': profile,
+        'profile': profile,
     }
     return render(request, 'explore.html', context)
 
+
 def profile(request, id_user):
-    user_object=User.objects.get(username=id_user)
-    profile=Profile.objects.get(user=request.user)
-    user_profile =Profile.objects.get(user=user_object)
-    user_stories=Story.objects.filter(user=id_user).order_by('-created_at')
-    user_stories_length=len(user_stories)
-    context={
+    user_object = User.objects.get(username=id_user)
+    profile = Profile.objects.get(user=request.user)
+    user_profile = Profile.objects.get(user=user_object)
+    user_stories = Story.objects.filter(user=id_user).order_by('-created_at')
+    user_stories_length = len(user_stories)
+    context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_stories': user_stories,
