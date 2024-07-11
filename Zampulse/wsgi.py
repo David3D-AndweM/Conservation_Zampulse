c515fe6d490c9ca2803c1c1d9ad71d0981a0076e
dabz <d.mwape@alustudent.com>
2024-07-11 05:51:49 +0300

    vom

--- Zampulse/wsgi.py
+++ Zampulse/wsgi.py
@@ -7,13 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+
+
+
 import os
-
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Zampulse.settings")
 
 application = get_wsgi_application()
-
-app = application  # Ensuring Gunicorn can find the application callable
+  # Ensuring Gunicorn can find the application callable
