3f96368541790609304949e8e301d4b25a4850d1
dabz <d.mwape@alustudent.com>
2024-07-11 05:00:56 +0300

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Zampulse.wsgi:application
--- Zampulse/settings.py
+++ Zampulse/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "your-secret-key"
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
