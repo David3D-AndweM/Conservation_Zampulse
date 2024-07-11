77a08cc005ed1b8da180ed11085db28e15765067
dabz <d.mwape@alustudent.com>
2024-07-11 06:36:46 +0300

    commited

--- Zampulse/settings.py
+++ Zampulse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split("")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 # Application definition
 
